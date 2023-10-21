6b62d26c23eef2718a56104fd89d3b6af2870409
Andrew Ward <andrew.c.bradley@gmail.com>
2023-10-21 13:41:23 -0400

    Get rid of default index page, move photos to index

--- src/Personal.Site/Pages/Index.cshtml.cs
+++ src/Personal.Site/Pages/Index.cshtml.cs
@@ -1,19 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using System.Web;
+using Amazon.S3;
+using Amazon.S3.Model;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Routing;
 
-namespace Personal.Site.Pages;
-
-public class IndexModel : PageModel
+public class PhotosModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
+    private readonly ILogger<PhotosModel> _logger;
+    private readonly IAmazonS3 _s3Client;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public PhotosModel(IAmazonS3 s3Client, ILogger<PhotosModel> logger)
     {
+        _s3Client = s3Client;
         _logger = logger;
     }
 
-    public void OnGet()
-    {
+    public List<string> ImagesToDisplay { get; private set; } = new List<string>();
 
+    public async Task OnGetAsync()
+    {
+        var response = await _s3Client.ListObjectsV2Async(new ListObjectsV2Request
+        {
+            BucketName = "bradley-ac-photos-resized",
+        });
+
+        foreach (var obj in response.S3Objects.Where(obj => obj.Key.EndsWith(".png", StringComparison.InvariantCultureIgnoreCase)))
+        {
+            ImagesToDisplay.Add(GetObjectUrl(obj));
+        }
+    }
+
+    public string GetObjectUrl(S3Object obj)
+    {
+        return $"https://{obj.BucketName}.s3.us-east-1.amazonaws.com/{HttpUtility.UrlEncode(obj.Key)}";
     }
 }
